a4a5b5ce4a7dd3b7af64520e104fade692358130
haqoffl <ananthametals14@gmail.com>
2024-08-25 15:17:14 +0530

    ..

--- app/user/signup/page.js
+++ app/user/signup/page.js
@@ -13,29 +13,33 @@ import { toast, ToastContainer } from 'react-toastify';
 const Page = () => {
   const router = useRouter();
   const [email, setEmail] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [password, setPassword] = useState('');
+  const [userName, setUserName] = useState('');
 
-  const handleResetPassword = async () => {
-    setLoading(true);
+  const handleCreateAccount = async () => {
     try {
       const response = await axios.post(
-        process.env.NEXT_PUBLIC_BACKEND_URL + 'resetPassword',
-        { email, newPassword }
+        process.env.NEXT_PUBLIC_BACKEND_URL + 'register/createUser',
+        {
+          email,
+          password,
+          userName,
+        }
       );
+      console.log(response);
 
-      if (response.status === 200) {
-        toast.success('Password reset successfully');
-        setTimeout(() => {
-          router.push('/user/login');
-        }, 3000);
-      } else {
-        toast.error('Failed to reset password');
+      if (response.status !== 200) {
+        toast.error('Error creating account');
+        router.push('/user/login');
+        return;
       }
+
+      toast.success('Account successfully created');
+      setTimeout(() => {
+        router.push('/user/login');
+      }, 3000);
     } catch (error) {
       toast.error('An error occurred. Please try again.');
-    } finally {
-      setLoading(false);
     }
   };
 
@@ -52,43 +56,58 @@ const Page = () => {
         </div>
         <div className="lg:w-5/12 md:w-6/12 h-screen bg-white flex flex-col items-center justify-center lg:px-32 px-20 gap-y-2">
           <div>
-            <h3 className="text-3xl font-bold">Reset Your Password</h3>
+            <h3 className="text-3xl font-bold">Create a user account</h3>
             <h1 className="text-blue text-4xl font-extrabold">Souldem</h1>
-            <p className="text-para">Enter your new password below</p>
+            <p className="text-para">
+              Have your Educational assets seamless with souldem
+            </p>
+          </div>
+
+          <div className="w-full flex flex-col gap-y-1">
+            <label>Your Name</label>
+            <Input
+              type="text"
+              placeholder="Your Good Name"
+              className="text-dark bg-gray"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
           </div>
 
           <div className="w-full flex flex-col gap-y-1">
             <label>Email</label>
             <Input
               type="email"
-              placeholder="Your email"
+              placeholder="email"
               className="text-dark bg-gray"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-
           <div className="w-full flex flex-col gap-y-1">
-            <label>New Password</label>
+            <label>Password</label>
             <Input
               type="password"
-              placeholder="New password"
+              placeholder="password"
               className="text-dark bg-gray"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
+          <p className="text-para text-xs">
+            By creating an account, I agree to Souldem's Terms of Service and
+            Privacy Policy.
+          </p>
           <Button
             className="bg-blue text-white p-2 px-4 rounded-l w-full"
-            onClick={handleResetPassword}
-            disabled={loading}
+            onClick={handleCreateAccount}
           >
-            Reset Password
+            Create Account
           </Button>
 
           <p>
-            Remembered your password?{' '}
+            Already have an account?{' '}
             <Link href="/user/login" className="text-blue">
               Login
             </Link>
